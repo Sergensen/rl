f61c952ebda936a7ee33d76739dbee3e4c24f2b8
Ilja Köthe <ilja.koethe@lhind.dlh.de>
2019-11-12 19:23:48 +0100

    changed impressum/legal. Added english translation of impressum to legal

--- src/main/Legal.js
+++ src/main/Legal.js
@@ -5,36 +5,57 @@ export default class Legal extends Component {
         return (
             <div className="container">
                 <div className="termctn">
-                        <p className="legal language">
-                            Die deutsche Version finden Sie <a className="linkcolor" href="impressum">hier</a>
-                        </p>
-                        <p className="header">Legal notice</p>
+                    <p className="legal language">
+                        Die deutsche Version finden Sie <a className="linkcolor" href="impressum">hier</a>
+                    </p>
+                    <p className="header">Legal notice</p>
 
-                        <p className="legal">
-                            Information according to the German § 5 telemedia-act<br /><br />
-                            Köthe und Sentürk GbR<br />
-                            Edewechter Landstraße 70<br />
-                            26131 Oldenburg<br /><br />
-                            Represented by:<br />
-                            Ilja Köthe, Sergen Sentürk<br /><br />
-                            Contact<br />
-                            Phone: +49 1525 6537719<br />
-                            E-Mail: info(at)richlist.net<br />
-                            Due to the small settlement of the USTG (Sales tax law) section 19 par. 1 sales tax is not calculated.<br /><br />
+                    <p className="legal">
+                        Information according to the German § 5 telemedia-act<br /><br />
+                        Köthe und Sentürk GbR<br />
+                        Edewechter Landstraße 70<br />
+                        26131 Oldenburg<br />
+                        Germany <br /><br />
+                        Represented by:<br />
+                        Ilja Köthe, Sergen Sentürk<br /><br />
+                        Contact<br />
+                        Phone: +49 1525 6537719<br />
+                        E-Mail: info(at)richlist.net<br />
+                        Due to the small settlement of the USTG (Sales tax law) section 19 par. 1 sales tax is not calculated.<br /><br />
 
-                            Source:<br />
-                            https://www.e-recht24.de<br />
-                            <p className="header">Disclaimer</p>
+                        <h2>dispute settlement</h2>
+                        <p>The European Commission provides a platform for online dispute resolution (OS): <a href="https://ec.europa.eu/consumers/odr" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr</a><br />
+                            You can find our e-mail address in the imprint above.</p>
+                        <p>We are not prepared or obliged to participate in dispute resolution proceedings before a consumer arbitration board.</p>
+                        <h3>Liability for content</h3>
+                        As a service provider we are responsible according to § 7 Abs.1 TMG (German Telemedia Act) for our own contents on these pages according to the general laws.
+                        According to §§ 8 to 10 TMG, we are not obliged as a service provider to monitor transmitted or stored third-party information or to investigate circumstances that indicate illegal activity.
+                        <br />
+                        Obligations to remove or block the use of information in accordance with general laws remain unaffected by this.
+                        A liability in this respect is however only possible from the time of knowledge of a concrete infringement.
+                        As soon as we become aware of such infringements, we will remove the content immediately.
 
-                            <p className="legal">Last updated: September 02, 2019</p>
-                            <p className="legal">The information contained on http://www.richlist.net website (the "Service") is for general information purposes only.</p>
-                            <p className="legal">Richlist assumes no responsibility for errors or omissions in the contents on the Service.</p>
-                            <p className="legal">In no event shall Richlist be liable for any special, direct, indirect, consequential, or incidental damages or any damages whatsoever, whether in an action of contract, negligence or other tort, arising out of or in connection with the use of the Service or the contents of the Service. Richlist reserves the right to make additions, deletions, or modification to the contents on the Service at any time without prior notice. This Disclaimer  for Richlist has been created with the help of <a className="linkcolor" href="https://www.termsfeed.com/">TermsFeed</a>.</p>
-                            <p className="legal">Richlist does not warrant that the website is free of viruses or other harmful components.</p>
+                        <h3>Liability for links</h3>
+                        Our offer contains links to external websites of third parties on whose contents we have no influence.
+                        Therefore, we cannot assume any liability for these external contents.
+                        The respective provider or operator of the pages is always responsible for the contents of the linked pages.
+                        The linked pages were checked for possible legal infringements at the time of linking. Illegal contents were not recognisable at the time of linking.
+                        <br />
+                        A permanent control of the contents of the linked pages is not reasonable without concrete evidence of an infringement.
+                        As soon as we become aware of any legal infringements, we will remove such links immediately.
+                        <h3>Copyright</h3>
+                        The contents and works on these pages created by the site operators are subject to German copyright law.
+                        Duplication, processing, distribution and any form of commercialization of such material beyond the scope of the copyright law shall require the prior written consent of its respective author or creator.
+                        Downloads and copies of these pages are only permitted for private, non-commercial use.
+                        <br />
+                        Insofar as the content on this site was not created by the operator, the copyrights of third parties are respected. 
+                        In particular, contents of third parties are marked as such. 
+                        Should you nevertheless become aware of a copyright infringement, please inform us accordingly. 
+                        As soon as we become aware of any infringements, we will remove such content immediately.
 
-                            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+                        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
-                        </p>
+                    </p>
                 </div>
             </div>
         );

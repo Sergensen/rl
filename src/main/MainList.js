1f5ccf17b30e64aae1c8ba5b33f0b0fd0d5f9591
Ilja <Boogeyman1337@hotmail.de>
2020-01-12 20:36:01 +0100

    fixed props. mainlist only shows when data loaded. added mainlist spinner

--- src/main/MainList.js
+++ src/main/MainList.js
@@ -10,6 +10,8 @@ import { Button } from 'react-bootstrap';
 import API from '../api';
 import { motion } from "framer-motion"
 import { delay } from '../Globals'
+import { Spinner } from 'react-bootstrap';
+
 
 export default class MainList extends Component {
 
@@ -113,38 +115,44 @@ export default class MainList extends Component {
         return (
             <div style={styles.main}>
                 <div style={styles.container}>
-                    <motion.div initial="hidden" animate="visible" variants={list} style={styles.listContainer}>
-                        <motion.div variants={list} style={styles.first}>
-                            <First user={data[0]} localProps={localProps[data[0] ? data[0].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                        </motion.div>
+                    {data.length > 0 ?
+                        <motion.div initial="hidden" animate="visible" variants={list} style={styles.listContainer}>
+                            <motion.div variants={list} style={styles.first}>
+                                <First user={data[0]} localProps={localProps[data[0] ? data[0].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                            </motion.div>
 
-                        <motion.div variants={list} style={styles.twoAndThree}>
-                            <Second user={data[1]} localProps={localProps[data[1] ? data[1].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                            <Third user={data[2]} localProps={localProps[data[2] ? data[2].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                        </motion.div>
+                            <motion.div variants={list} style={styles.twoAndThree}>
+                                <Second user={data[1]} localProps={localProps[data[1] ? data[1].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                                <Third user={data[2]} localProps={localProps[data[2] ? data[2].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                            </motion.div>
 
-                        <motion.div variants={list} style={styles.fourToSix}>
-                            {/* {this.render4to6()} */}
-                            <ListElement position={4} user={data[3]} localProps={localProps[data[3] ? data[3].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                            <ListElement position={5} user={data[4]} localProps={localProps[data[4] ? data[4].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                            <ListElement position={6} user={data[5]} localProps={localProps[data[5] ? data[5].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                        </motion.div>
+                            <motion.div variants={list} style={styles.fourToSix}>
+                                {/* {this.render4to6()} */}
+                                <ListElement position={4} user={data[3]} localProps={localProps[data[3] ? data[3].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                                <ListElement position={5} user={data[4]} localProps={localProps[data[4] ? data[4].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                                <ListElement position={6} user={data[5]} localProps={localProps[data[5] ? data[5].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                            </motion.div>
 
-                        <motion.div variants={list} style={styles.sevenToNine} >
-                            {/* {this.render7to10()} */}
-                            <ListElement position={7} user={data[6]} localProps={localProps[data[6] ? data[6].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                            <ListElement position={8} user={data[7]} localProps={localProps[data[7] ? data[7].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                            <ListElement position={9} user={data[8]} localProps={localProps[data[8] ? data[8].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
-                            <ListElement position={10} user={data[9]} localProps={localProps[data[9] ? data[9].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                            <motion.div variants={list} style={styles.sevenToNine} >
+                                {/* {this.render7to10()} */}
+                                <ListElement position={7} user={data[6]} localProps={localProps[data[6] ? data[6].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                                <ListElement position={8} user={data[7]} localProps={localProps[data[7] ? data[7].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                                <ListElement position={9} user={data[8]} localProps={localProps[data[8] ? data[8].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                                <ListElement position={10} user={data[9]} localProps={localProps[data[9] ? data[9].uniqueName : null]} addPropsToUser={this.addPropsToUser.bind(this)} />
+                            </motion.div>
                         </motion.div>
-                    </motion.div>
+                        :
+                        <div style={styles.placeHolder}>
+                            <Spinner animation="border" role="status" variant="light" style={styles.spinner} />
+                        </div>
+                    }
 
                     <motion.div initial={false} whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95, x: "-5px", y: "5px" }} style={styles.buttonContainer}>
                         <Button onClick={() => window.location.href = '/pay'} variant="primary" size="lg" block style={styles.payButton}>
                             💸 Get on the list 💸
                         </Button>
                     </motion.div>
-                    
+
                     <div style={styles.InfoSection}>
                         <InfoSection />
                     </div>
@@ -172,9 +180,20 @@ const styles = {
         maxWidth: 500,
         display: "flex",
         flexDirection: "column",
-        margin: '1rem 0'
+        margin: '1rem 0',
         //justifyContent: "center",
         //alignItems: "center",
+    },
+    placeHolder: {
+        width: "100%",
+        height: 200,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    spinner:{
+        width: 150,
+        height: 150,
     },
     listContainer: {
         width: "100%",

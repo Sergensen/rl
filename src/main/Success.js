9444caf86a4a8c831615094dd0c06c3d11485bbc
Sergen <sergen.sentuerk@web.de>
2020-01-27 15:06:21 +0100

    succes / progress page - kann zusammengeführt werden

--- src/main/Success.js
+++ src/main/Success.js
@@ -5,12 +5,20 @@ export default class Success extends Component {
         return (
             <div style={styles.container}>
                 <div style={styles.success} className="specialbody">
-                    <h1 id="paytext">Payment successful, check your Email inbox!</h1> <br />
-                    <h1 id="paytext">Download the app and check your rank!</h1> <br />
-                    <a style={{height: 50}} href="pay" id="paytext" class="link">Or go back</a>
+                    <h1 id="paytext">
+                        Thank you for your purchase, we are processing your payment now.
+                        You will receive a confirmation email so check your inbox!
+                    </h1>  <br />
+                    <h1 id="paytext">
+                        This process can take up to 10 minutes. 
+                        If you don't receive a confirmation email you wont be charged.
+                    </h1> <br />
+                    <h1 id="paytext">
+                        Your confirmation email will contain your personal payment key. 
+                        You will need it to make additional payments.
+                    </h1>
                 </div>
             </div>
-            
         );
     }
 }
@@ -20,11 +28,16 @@ const styles = {
         display: "flex",
         justifyContent: "center",
         height: "80vh",
+        backgroundColor: "rgba(0, 0, 0, 0.5)"
     },
     success: {
-        height: "100vh",
         flex: 1,
         paddingLeft: 10,
         paddingRight: 10,
+        border: "1px solid grey",
+        borderWidth: "1px 0 1px 0",
+        backgroundColor: "black",
+        maxWidth: 900,
+        width: "100%",
     }
 }

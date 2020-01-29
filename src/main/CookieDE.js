287d89ae935897fb246507411aea357e0d299653
Sergen <sergen.sentuerk@web.de>
2020-01-29 16:31:08 +0100

    cookie

--- src/main/CookieDE.js
+++ src/main/CookieDE.js
@@ -6,15 +6,54 @@ export default class CookieDE extends Component {
             <div className="container">
                 <div className="termctn">
                     <p className="terms language">
-                            <a className="linkcolor" href="en">You can find the English version here.</a>
+                        <a className="linkcolor" href="en">You can find the English version here.</a>
                     </p>
                     <br />
                     <p className="header">
-                            RichList Cookie-Richlinie
-                    </p>
+                        RichList Cookie-Richlinie
+                    </p><br />
                     <p className="terms">Zuletzt bearbeitet am 10. Januar 2020</p>
+                    <p className="terms">Mit dieser Cookie-Richtlinie informiert der Website-Betreiber den Nutzer der Website über den Einsatz von Cookies oder ähnlichen Speichertechnologien (nachfolgend „Cookies“) auf dieser Website.</p><br /><br /><br />
 
-                    <p className="terms"></p>
+                    <p className="terms">1. Was sind Cookies?</p> <br /><br />
+                    <p className="terms">Cookies sind kleine Textdateien, die vom Webbrowser auf dem Endgerät des Nutzers zur Speicherung von bestimmten Informationen abgelegt werden. Beim nächsten Aufruf unserer Website mit demselben Endgerät werden die in Cookies gespeicherten Informationen an unsere Website („First Party Cookie“) oder eine andere Website, zu der das Cookie gehört („Third Party Cookie“), zurückgesandt. Durch die im Cookie gespeicherten und zurückgesandten Informationen erkennt die jeweilige Website, dass der Nutzer diese mit dem Webbrowser seines Endgeräts bereits aufgerufen und besucht hat. Diese Informationen nutzen wir, um dem Nutzer die Website gemäß seinen Präferenzen optimal anzeigen zu können. Es wird hierbei aber nur das Cookie selbst auf dem Endgerät identifiziert. Eine darüber hinausgehende Speicherung von personenbezogenen Daten erfolgt nur, wenn der Nutzer uns seine ausdrückliche Zustimmung dafür gibt oder wenn diese Speicherung unbedingt erforderlich ist, um den angebotenen und aufgerufenen Dienst nutzen zu können.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">2. Zustimmung zur Verwendung von Cookies</p> <br /><br />
+                    <p className="terms">Cookies, die nicht unbedingt erforderlich sind, um die Dienste auf dieser Website zur Verfügung zu stellen, werden erst nach Einwilligung verwendet. Indem der Nutzer auf Grundlage eines von uns auf der Website erteilten Hinweises („Cookie-Banner“) eine Einwilligung zur Verwendung von Cookies erteilt hat, stimmt er der Verwendung von Cookies zu. Seine Cookie Einstellungen kann der Nutzer jederzeit individuell anpassen, indem er zum Beispiel einzelne Cookie-Kategorien aktiviert oder deaktiviert. Wie der Nutzer Cookies generell (inklusive der unbedingt erforderlichen Cookies) in seinem Browser deaktivieren oder löschen kann, wird unter dem Punkt "Deaktivierung oder Löschung sämtlicher Cookies" beschrieben.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">3. Rechtsgrundlage</p> <br /><br />
+                    <p className="terms">Wenn der Nutzer auf Grundlage eines von uns auf der Website erteilten Hinweises („Cookie-Banner“) eine Einwilligung zur Verwendung von Cookies erteilt hat, richtet sich die Rechtmäßigkeit der Datenverarbeitung nach Art. 6 Abs. 1 S. 1 lit. a DSGVO. Wenn keine Einwilligung angefordert wird, stellt unser berechtigtes Interesse (d. h. Interesse an der Analyse, Optimierung und dem wirtschaftlichen Betrieb dieser Website und Leistungen) im Sinne des Art. 6 Abs. 1 S. 1 lit. f DSGVO die Rechtsgrundlage für die Nutzung der Cookies dar.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4. Kategorien der verwendeten Cookies</p> <br /><br />
+                    <p className="terms">Je nach Verwendungszweck und Funktion unterteilen wir die von der Website verwendeten Cookies in die folgenden Kategorien:<br />
+                        - Unbedingt erforderliche Cookies;<br />
+                        - Funktionale-Cookies;<br />
+                        - Performance-Cookies;<br />
+                        - Marketing- / Third Party- / Zustimmungspflichtige-Cookies</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.1 Unbedingt erforderliche Cookies</p> <br /><br />
+                    <p className="terms">Unbedingt erforderliche Cookies sorgen dafür, dass diese Website, beziehungsweise der Dienst, ordnungsgemäß funktioniert. Das heißt, ohne diese Cookies ist diese Website, beziehungsweise der Dienst, nicht wie vorgesehen nutzbar. Diese Art von Cookies wird ausschließlich von dem Betreiber der Website verwendet (First Party Cookies) und sämtliche Informationen, die in den Cookies gespeichert sind, werden nur an diese Website gesendet. Unbedingt erforderliche Cookies können nicht einzeln deaktiviert bzw. aktiviert werden. Der Nutzer hat jederzeit die Möglichkeit, seine Cookie-Einstellungen anzupassen oder Cookies generell in seinem Browser zu deaktivieren (siehe Punkt „Deaktivierung oder Löschung sämtlicher Cookies").</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.2 Funktionale Cookies</p> <br /><br />
+                    <p className="terms">Funktionale Cookies ermöglichen dieser Webseite, getätigte Angaben, wie z. B. den Benutzernamen oder die Sprachauswahl, zu speichern und dem Nutzer darauf basierend verbesserte und personalisierte Funktionen anzubieten. Diese Cookies sammeln und speichern ausschließlich anonymisierte Informationen. Der Nutzer kann der Verwendung von Funktionalen Cookies jederzeit widersprechen, indem er seine Cookie-Einstellungen entsprechend anpasst.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.3 Performance Cookies</p> <br /><br />
+                    <p className="terms">Performance-Cookies sammeln Informationen darüber, wie diese Websites genutzt wird, um darauf basierend deren Attraktivität, Inhalt und Funktionalität zu verbessern. Diese Cookies helfen uns beispielsweise festzustellen, ob und welche Unterseiten dieser Website besucht werden und für welche Inhalte sich die Nutzer besonders interessieren. Wir erfassen die Anzahl der Zugriffe auf eine Seite, die Anzahl der aufgerufenen Unterseiten, die auf der Website verbrachte Zeit, die Reihenfolge der besuchten Seiten, welche Suchbegriffe unsere Nutzer zu uns geführt haben, das Land, die Region und gegebenenfalls die Stadt, aus der der Zugriff erfolgt. Zudem erfassen wir Bewegungen, z. B. „Klicks“ und das Scrollen mit der Computermaus, um zu verstehen, welche Bereiche dieser Website unsere Nutzer besonders interessieren. Als Resultat können wir die Inhalte dieser Website gezielter auf die Bedürfnisse unserer Nutzer abstimmen und unser Angebot verbessern. Die übermittelte IP-Adresse eines Rechners wird automatisch anonymisiert und ermöglicht uns keinen Rückschluss auf den einzelnen Nutzer. Wir behalten uns vor, Informationen, die wir mittels Cookies aus einer anonymisierten Analyse des Nutzungsverhaltens von Nutzern der Websites gewonnen haben, auch zu nutzen, um spezifische Werbung für unsere Produkte anzuzeigen. Wir sind der Auffassung, dass der Nutzer hiervon profitiert, weil wir Werbung oder Inhalte einblenden, von denen wir aufgrund des Surf-Verhaltens annehmen, dass sie zu den Interessen des Nutzers passen. Der Nutzer der Website hat das Recht, aus Gründen, die sich aus seiner besonderen Situation ergeben, bei der Verarbeitung ihn betreffender personenbezogener Daten, die zu statistischen Zwecken gem. Art. 89 Abs. 1 DSGVO erfolgt, zu widersprechen. Zur Wahrnehmung dieser und anderer Betroffenenrechte, kann sich ein Betroffener jederzeit an die unten angegebene Kontaktadresse wenden. Der Nutzer der Website kann der Verwendung von Performance-Cookies jederzeit widersprechen, indem er seine Cookie-Einstellungen entsprechend anpasst.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.4 Marketing- / Third Party-Cookies</p> <br /><br />
+                    <p className="terms">Marketing- / Third Party- / Zustimmungspflichtige-Cookies stammen unter anderem von externen Werbeunternehmen und werden verwendet, um Informationen über die vom Nutzer besuchten Websites zu sammeln, um z. B. zielgruppenorientierte Werbung für den Benutzer zu erstellen. Der Nutzer kann der Verwendung von Marketing-Cookies jederzeit widersprechen, indem er seine Cookie-Einstellungen entsprechend anpasst.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">5. Deaktivierung oder Löschung sämtlicher Cookies</p> <br /><br />
+                    <p className="terms">Der Nutzer kann seinen Webbrowser so einstellen, dass das Speichern von Cookies auf seinem Endgerät generell verhindert wird beziehungsweise er jedes Mal gefragt wird, ob er mit dem Setzen von Cookies einverstanden ist. Einmal gesetzte Cookies kann der Nutzer jederzeit wieder löschen. Wie das Ganze funktioniert, wird in Hilfe-Funktion des jeweiligen Webbrowsers beschrieben. Eine generelle Deaktivierung von Cookies kann gegebenenfalls zu Funktionseinschränkungen dieser Website führen.</p>
+                    <br /><br /><br />
                 </div>
             </div>
         );
--- src/main/CookieEN.js
+++ src/main/CookieEN.js
@@ -6,15 +6,55 @@ export default class CookieEN extends Component {
             <div className="container">
                 <div className="termctn">
                     <p className="terms language">
-                            <a className="linkcolor" href="de">Die deutsche Verion finden Sie hier.</a>
+                        <a className="linkcolor" href="de">Die deutsche Verion finden Sie hier.</a>
                     </p>
                     <br />
                     <p className="header">
-                            RichList Cookie-Policy
+                        RichList Cookie-Policy
                     </p>
                     <p className="terms">Last Updated on January 10, 2020</p>
 
-                    <p className="terms"></p>
+                    <p className="terms">With this cookie policy, we wish to inform you about the use of cookies or similar storage technologies (hereinafter “cookies”) on this website.</p><br /><br /><br />
+
+                    <p className="terms">1. What are Cookies?</p> <br /><br />
+                    <p className="terms">Cookies are small text files that are stored by your browser on your device to save certain information or image files, such as pixels. The next time you visit our website on the same device, the information saved in the cookies will subsequently be transmitted either to our website (“First Party Cookie”) or to another website to which the cookie belongs (“Third Party Cookie”). Through the information saved and returned, the respective website recognizes that you have already accessed and visited it with the browser you use on that device. We use this information to be able to design and display the website in an optimum way in line with your preferences. In that respect, only the cookie itself is identified on your device. Beyond this extent, your personal data will only be saved upon your express consent or if it is strictly necessary to be able to use the service offered to and accessed by you accordingly.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">2. Consent to the use of Cookies</p> <br /><br />
+                    <p className="terms">Cookies that are not strictly necessary to make the services available on our website are only used when you provide us with your consent. As soon as you actively accept the use of cookies via the cookie banner on our website, you consent to the use of cookies. You can, of course, customize your cookie settings at any time, for example by activating or deactivating individual cookie categories. You can find out how to deactivate or delete cookies in general (i.e. including the essential cookies) in your browser under the last point of this Cookie Policy.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">3. Legal Basis</p> <br /><br />
+                    <p className="terms">Where you have provided consent to the use of cookies on the basis of a notice (Cookie banner) displayed on our websites (“cookies banner”), the legality of this data processing is governed by Art. 6 (1) (a) GDPR. Where consent is not requested, the legal basis for the use of these functional cookies is a legitimate interest (i.e. interest in the analysis, optimization and economic operation of the website and services) within the meaning of Art. 6 (1) (f) GDPR).</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4. Cookie Categories</p> <br /><br />
+                    <p className="terms">We divide the cookies we use into the following categories based on their purpose and function: <br />
+                        - Strictly Necessary Cookies; <br />
+                        - Functional Cookies; <br />
+                        - Performance Cookies; as well as <br />
+                        - Marketing / Third Party / Consent Based Cookies</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.1 Strictly Necessary Cookies</p> <br /><br />
+                    <p className="terms">Strictly necessary cookies guarantee functions without which you cannot use this web page and service as intended. These cookies are used exclusively by this website and are therefore first party cookies. This means that all information stored in the cookies will be returned to this website. Strictly necessary cookies cannot be activated or deactivated individually. However, you can adjust your cookie preferences via the Cookie Settings or deactivate all cookies in your browser at any time (see below ”Deactivation or deletion of all cookies”).</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.2 Functional Cookies</p> <br /><br />
+                    <p className="terms">Functional cookies enable our website to store information already provided (such as registered name or language selection) and to offer you improved and more personalized functions based on this information. These cookies collect and store only anonymous information. You can opt out of using Functional Cookies at any time by adjusting your Cookie Settings accordingly.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.3 Performance Cookies</p> <br /><br />
+                    <p className="terms">Performance cookies collect information about how our websites are used in order to improve their attractiveness, content and functionality. These cookies help us, for example, to determine whether and which subpages of our website are visited and in which content you are particularly interested. In particular, we record the number of visits to a page, the number of subpages accessed, the time spent on our website, the order of the pages visited, which search terms led you to us, the country, region and, if applicable, the city from which access is made, and the proportion of mobile devices accessing our websites. We also capture movement, clicks and scrolling with the computer mouse to understand which areas of our website are of particular interest to you. As a result, we can tailor the content of our website more specifically to your needs and optimize our offering. The IP address of your computer transmitted for technical reasons is automatically anonymized and does not allow us to draw any conclusions about you, as an individual user. We reserve the right to use information that we have obtained through cookies from an anonymous analysis of the usage behavior of visitors to our websites to display specific advertisements for certain products on our own websites. We believe that you, as a user, benefit from this because we display advertisements or content that, based on your browsing habits, in our option, matches your interests. This will allow you to see less randomly distributed ads or content that may interest you less. You also have the right to object to the processing of personal data that is used for statistical purposes in accordance with Art. 89 para. 1 GDPR for reasons that arise from your particular situation. In order to exercise the aforementioned and other data subject rights, the affected party may, at any time, contact the site owner via the contact address provided in the imprint or privacy policy. You can opt out of using Performance Cookies at any time by adjusting your Cookie Settings accordingly.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">4.4 Marketing / Third Party Cookies</p> <br /><br />
+                    <p className="terms">Marketing / Third Party Cookies originate from external advertising companies (among others) and are used to gather information about the websites visited by you, in order to e.g. create targeted advertising for you. You can withdraw your consent to the use of Marketing / Third Party Cookies individually and at any time with effect for the future by adjusting your Cookie Settings accordingly.</p>
+                    <br /><br /><br />
+
+                    <p className="terms">5. Deactivation or deletion of all cookies</p> <br /><br />
+                    <p className="terms">You can set your web browser in such a way that cookies are generally prevented from being saved to your device and/or that you are asked each time whether you are in agreement with cookies being enabled. You can also at any time delete cookies that have been enabled again. You can find out how all this works in detail via your browser’s help function. Please note that generally deactivating cookies may lead to functional restrictions on our website.</p>
+                    <br /><br /><br />
                 </div>
             </div>
         );
